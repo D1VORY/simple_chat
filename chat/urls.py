c057fd67f9a3c209e3c0012ddb798fff37e3f1b6
D1VORY <dubchakob@gmail.com>
2020-09-24 01:58:54 +0300

    added  auth system

--- chat/urls.py
+++ chat/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from .views import *
+from . import views
 
 urlpatterns = [
-    path('', index)
+    path('', views.index, name='index'),
+    path('login/', views.loginPage, name = 'login'),
+    path('register/', views.registerPage, name = 'register'),
+    path('logout/', views.logoutUser, name = 'logout'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Message
-# Create your views here.
 
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from .decorators import unauthenticated_user
+from .forms import CreateUserForm
+from django.contrib.auth.decorators import login_required
 
-
+@login_required(login_url='login/')
 def index(request):
     template = 'chat/chatroom.html'
     messages = reversed(Message.objects.all().order_by('-timestamp'))
     return render(request, template, context = {'messages':messages})
+
+
+@unauthenticated_user
+def registerPage(request):
+    form = CreateUserForm()
+
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account was created for {username}')
+            return redirect('login')
+
+    context = {'form':form}
+    return render(request, 'chat/register.html', context)
+
+
+@unauthenticated_user
+def loginPage(request):
+    context = {}
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username , password = password)
+
+        if user is not None :
+            login(request,user)
+            return redirect('index')
+        else:
+            messages.info(request, 'username or password is incorrect')
+            return render(request, 'chat/login.html', context)
+
+    return render(request, 'chat/login.html', context)
+
+
+def logoutUser(request):
+    logout(request)
+    return redirect('login')
